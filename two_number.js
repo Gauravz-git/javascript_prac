a989c80c458e4bed017a3b405a8bebd61abc82d0
gauravz-git <gauravc0223@gmail.com>
2026-04-26 12:34:03 +0530

    second js program

---
+++ two_number.js
@@ -0,0 +1,18 @@
+/* Store two numbers and print their:
+
+sum
+difference
+product
+*/
+
+let num1 = 1;
+let num2 = 6;
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let product = num1 * num2;
+
+
+console.log("sum: " + sum);
+console.log("difference: " + diff);
+console.log("product: " + product);
